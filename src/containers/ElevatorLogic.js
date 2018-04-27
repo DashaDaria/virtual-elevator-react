778e573811f02c5b5ccd5b455bdbf114a6d39ced
DashaDaria <dashadariallc@gmail.com>
2018-04-27 11:32:21 -0600

    setting up the app structure

---
+++ src/containers/ElevatorLogic.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class ElevatorLogic extends Component {
+
+  render() {
+    return (
+      <div className="App">
+
+        <header className="App-header">
+          <h1 className="App-title">Welcome to the Smart Virtual Elevator</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default ElevatorLogic;
