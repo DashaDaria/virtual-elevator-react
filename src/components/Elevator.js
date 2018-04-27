4ab07bda514dc2c641345fe644f1176cc633c9cf
DashaDaria <dashadariallc@gmail.com>
2018-04-27 14:23:21 -0600

    visual representation of a building with an elevator as a table

--- src/components/Elevator.js
+++ src/components/Elevator.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const Elevator = () => {
+const Elevator = ({ currentFloor }) => {
 
   return(
-    <div>hi</div>
+    <div className="building-container">
+        <table>
+        <tbody>
+          <tr>
+            <td>10</td>
+          </tr>
+          <tr>
+            <td>9</td>
+          </tr>
+          <tr>
+            <td>8</td>
+          </tr>
+          <tr>
+            <td >7</td>
+          </tr>
+          <tr>
+            <td>6</td>
+          </tr>
+          <tr>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>1</td>
+          </tr>
+          </tbody>
+        </table>
+    </div>
   )
 }
 
